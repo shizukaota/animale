1dab78e6349600bb2c62a0affb2f9cee5a23ed66
shizuka ota <shizukaota@shizukanoMacBook-Pro.local>
2019-05-28 13:48:25 +0900

    aws deploy

--- db/migrate/20190512041624_create_menus.rb
+++ db/migrate/20190512041624_create_menus.rb
@@ -1,6 +1,6 @@
 class CreateMenus < ActiveRecord::Migration[5.2]
   def change
-    create_table :menus do |t|
+    create_table :menus, :options => 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
       t.references :store, foreign_key: true
       t.string :drink
       t.string :food
